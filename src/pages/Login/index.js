ae418b973aaa5f621da19315b7aefd2dc4b86e14
Arthur <arthurarp@hotmail.com>
2020-10-29 00:33:56 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#38 Adicionando elementos na tela de login de acordo com o prototipo de alta fidelidade

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,82 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View,
+        Text,
+        Image,
+        TextInput,
+        TouchableOpacity, 
+    } from 'react-native';
+
+import styles from './styles';
+import logo from '../../assets/logo.png';
 
 export default function Login() {
     return(
-        <View>
-            <Text>
-                Tela de login
-            </Text>
+        <View style={styles.background}>
+            <View style={styles.top}>
+                <Image 
+                    style={styles.logo}
+                    source={logo}
+                />
+            </View>
+            <View style={styles.loginWithContainer}>
+                <TouchableOpacity
+                    style={styles.btnRegister}
+                    onPress={() => {}}
+                >
+                    <Text>Facebook</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.btnRegister}
+                    onPress={() => {}}
+                >
+                    <Text>Google</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.btnRegister}
+                    onPress={() => {}}
+                >
+                    <Text>Apple</Text>
+                </TouchableOpacity>
+
+            </View>
+            <View style={styles.body}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="E-mail"
+                    autoCorrect={false}
+                    onChangeText={() => {}}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Senha"
+                    autoCorrect={false}
+                    secureTextEntry={true}
+                    onChangeText={() => {}}
+                />
+
+                <TouchableOpacity
+                    style={styles.btnRegister}
+                    onPress={() => {}}
+                >
+                    <Text>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.bottom}>
+                <TouchableOpacity
+                    style={styles.btnRegister}
+                    onPress={() => {}}
+                >
+                    <Text>INSCREVER-SE</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.btnRegister}
+                    onPress={() => {}}
+                >
+                    <Text>RECUPERAR SENHA</Text>
+                </TouchableOpacity>
+            </View>
         </View>
+          
+      
     );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,9 +1,58 @@
- 
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 export default StyleSheet.create({
     background: {
       flex: 1,
-      backgroundColor: '#9dcdf5',
+      backgroundColor: '#ff0000'
+    },
+    top: {
+      flex: 0.3,
+      alignItems: 'center',
+      justifyContent: 'center',
+      // backgroundColor: '#00ff00',
+      // height: '33.3333%',
+    },
+    logo: {
+      width: windowWidth * .20,
+      height: windowWidth * .20,
+    },
+    topMainText: {},
+    loginWithContainer: {
+      flex: 0.4,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    body: {
+      flex: 0.8,
+      alignItems: 'center',
+      justifyContent: 'center',
+      // backgroundColor: '#0000ff',
+      // height: '33.3333%',
+    },
+    bodyMainText: {},
+    inputsContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      // backgroundColor: '#ff0000',
+      // height: '33.3333%',
+    },
+    bottom: {
+      flex: 0.3,
+      alignItems: 'center',
+      justifyContent: 'center',
+      // backgroundColor: '#000'
+    },
+    bottomMainText: {},
+    input: {
+      backgroundColor: 'white',
+      borderWidth: 1,
+      width: '85%',
+      height: '15%',
+      color: '#FFF',
+      marginTop: 15,
     },
 })
