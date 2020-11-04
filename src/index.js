9cf57b682a9049cf2f6f30f914ccc13857f6e075
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-11-03 22:46:12 -0300

    [FEATURE] adiciona routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
 
+import Routes from './routes';
 export default function app() {
   return (
     <>
-      <Text>teste</Text>
+      <Routes />
     </>
   );
 }
