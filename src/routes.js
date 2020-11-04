53fb7e28e1073298ee05c8613e0b0b516f758fc6
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-11-03 23:31:21 -0300

    [FEATURE] Adiciona navbar UnBArqDsw/2020.1_G6_Pomo#50

--- src/routes.js
+++ src/routes.js
@@ -3,29 +3,58 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import Main from './pages/Main';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
 function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Main"
-        screenOptions={{
-          gestureEnabled: false,
-          headerTitleAlign: 'center',
-          headerStyle: {
-            backgroundColor: '#7159c1',
+      <Tab.Navigator
+        tabBarOptions={{
+          resetOnBlur: true,
+          activeTintColor: '#fff',
+          keyboardHidesTabBar: true,
+          inactiveTintColor: 'rgba(255,255,255,0.6)',
+          style: {
+            backgroundColor: '#8d41a8',
           },
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          headerTintColor: '#fff',
-
-          headerBackTitleVisible: false,
         }}>
-        <Stack.Screen name="Main" component={Main} options={{title: 'Main'}} />
-      </Stack.Navigator>
+        <Tab.Screen
+          name="Dashboard"
+          component={Main}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Icon color={color} size={20} name="event" />
+            ),
+            tabBarLabel: 'Agendamentos',
+          }}
+        />
+        <Tab.Screen
+          name="New"
+          component={Main}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Icon color={color} size={20} name="add-circle-outline" />
+            ),
+            tabBarLabel: 'Agendar',
+            tabBarVisible: false,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Main}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Icon color={color} size={20} name="person" />
+            ),
+            tabBarLabel: 'Meu Perfil',
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
