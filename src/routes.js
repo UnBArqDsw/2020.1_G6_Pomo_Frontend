930d4d0740a9b649443494d3aa25f5cc2bb8a01d
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-11-04 18:51:22 -0300

    [FEATURE] adiciona cores  UnBArqDsw/2020.1_G6_Pomo#50

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+// importa as telas
 import Main from './pages/Main';
 
 const Tab = createBottomTabNavigator();
@@ -16,29 +17,36 @@ function Routes() {
       <Tab.Navigator
         tabBarOptions={{
           resetOnBlur: true,
-          activeTintColor: '#fff',
+          activeTintColor: '#e91e63',
           keyboardHidesTabBar: true,
-          inactiveTintColor: 'rgba(255,255,255,0.6)',
+          inactiveTintColor: '#000000',
+          showLabel: false,
           style: {
-            backgroundColor: '#8d41a8',
+            backgroundColor: '#e0e0e0',
+            borderTopLeftRadius: 26,
+            borderTopRightRadius: 26,
+            position: 'absolute',
+            bottom: 0,
+            padding: 10,
           },
         }}>
+        {/* cada tabScreen e um icone da navbar. so mudar o nome do compoente */}
         <Tab.Screen
-          name="Dashboard"
+          name="Main"
           component={Main}
           options={{
             tabBarIcon: ({color}) => (
-              <Icon color={color} size={20} name="event" />
+              <Icon color={color} size={32} name="playlist-add-check" />
             ),
-            tabBarLabel: 'Agendamentos',
           }}
         />
         <Tab.Screen
           name="New"
           component={Main}
           options={{
-            tabBarIcon: () => <Icon size={20} name="timer" />,
-            tabBarLabel: 'time',
+            tabBarIcon: ({color}) => (
+              <Icon color={color} size={32} name="stacked-line-chart" />
+            ),
           }}
         />
         <Tab.Screen
@@ -46,7 +54,25 @@ function Routes() {
           component={Main}
           options={{
             tabBarIcon: ({color}) => (
-              <Icon color={color} size={20} name="person" />
+              <Icon color={color} size={32} name="timer" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="x"
+          component={Main}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Icon color={color} size={32} name="groups" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="xx"
+          component={Main}
+          options={{
+            tabBarIcon: ({color}) => (
+              <Icon color={color} size={32} name="person" />
             ),
             tabBarLabel: 'Meu Perfil',
           }}
