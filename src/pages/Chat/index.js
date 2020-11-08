c4321db54c9ac64973de13f7d792ee30e8ffdb6d
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-11-07 21:34:16 -0300

    [FEATURE]  cria pagina de chat UnBArqDsw/2020.1_G6_Pomo#47

---
+++ src/pages/Chat/index.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container} from './styles';
+export default function Menssage() {
+  const [chats, setChat] = useState([]);
+  return <Container></Container>;
+}
