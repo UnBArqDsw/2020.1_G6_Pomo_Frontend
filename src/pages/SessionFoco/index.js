9b2238f7d6ea1f2916ca2410fa6e317f8215623c
thallysbraz <thallysbraz3@gmail.com>
2020-11-11 14:51:49 -0300

    iniciando tela de session Foco

---
+++ src/pages/SessionFoco/index.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Keyboard,
+  Animated,
+  Dimensions,
+  StatusBar,
+} from 'react-native';
+
+import styles from './styles';
+import logo from '../../assets/logo.png';
+import Background from '../../components/Background';
+
+export default function SessionFoco() {
+  return (
+    <Background>
+      <h1>Hello word</h1>
+    </Background>
+  );
+}
