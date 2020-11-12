8203e9c58ca1524ca699308acec8c240fea91581
thallysbraz <thallysbraz3@gmail.com>
2020-11-12 19:44:14 -0300

    página de session foco

--- src/pages/SessionFoco/index.js
+++ src/pages/SessionFoco/index.js
@@ -12,14 +12,66 @@ import {
   StatusBar,
 } from 'react-native';
 
-import styles from './styles';
-import logo from '../../assets/logo.png';
-import Background from '../../components/Background';
+import {ItemContainer, ItemTitle, ItemDescription, Container} from './styles';
+import {Grid, Row, Col} from 'react-native-easy-grid';
+import Icon from 'react-native-vector-icons/Feather';
+import SearchBar from 'react-native-search-bar';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function SessionFoco() {
   return (
-    <Background>
-      <h1>Hello word</h1>
-    </Background>
+    <>
+      <Container>
+        <SearchBar placeholder="Buscar" />
+        <Grid>
+          <Col>
+            <Row>
+              <ItemContainer>
+                <Icon name="book" size={30} color="#fff" />
+                <ItemTitle>Title</ItemTitle>
+                <ItemDescription>Description</ItemDescription>
+              </ItemContainer>
+            </Row>
+            <Row>
+              <ItemContainer>
+                <Icon name="book" size={30} color="#fff" />
+                <ItemTitle>Title</ItemTitle>
+                <ItemDescription>Description</ItemDescription>
+              </ItemContainer>
+            </Row>
+            <Row>
+              <ItemContainer>
+                <Icon name="book" size={30} color="#fff" />
+                <ItemTitle>Title</ItemTitle>
+                <ItemDescription>Description</ItemDescription>
+              </ItemContainer>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <ItemContainer>
+                <Icon name="book" size={30} color="#fff" />
+                <ItemTitle>Title</ItemTitle>
+                <ItemDescription>Description</ItemDescription>
+              </ItemContainer>
+            </Row>
+            <Row>
+              <ItemContainer>
+                <Icon name="book" size={30} color="#fff" />
+                <ItemTitle>Title</ItemTitle>
+                <ItemDescription>Description</ItemDescription>
+              </ItemContainer>
+            </Row>
+            <Row>
+              <ItemContainer>
+                <Icon name="book" size={30} color="#fff" />
+                <ItemTitle>Title</ItemTitle>
+                <ItemDescription>Description</ItemDescription>
+              </ItemContainer>
+            </Row>
+          </Col>
+        </Grid>
+      </Container>
+    </>
   );
 }
