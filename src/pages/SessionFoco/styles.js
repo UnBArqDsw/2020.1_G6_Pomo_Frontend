a1a9082297c4eca12b81d545a77c522097157acb
thallysbraz <thallysbraz3@gmail.com>
2020-11-12 19:43:05 -0300

    adicionando style do ItemContainer

--- src/pages/SessionFoco/styles.js
+++ src/pages/SessionFoco/styles.js
@@ -4,3 +4,13 @@ export const Container = styled.View`
   background-color: #fff;
   flex: 1;
 `;
+export const ItemContainer = styled.View`
+  background-color: #7159c1;
+  margin: 5px;
+  padding: 15px;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  border-radius: 50px;
+`;
