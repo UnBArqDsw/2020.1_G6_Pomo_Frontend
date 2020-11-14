dfe8abad329e8e7c5d9b78525ec6e4743141c5fe
Arthur <arthurarp@hotmail.com>
2020-11-14 15:22:25 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#53 Adicionando componentes da tela de recuperar senha

--- src/pages/Recovery/index.js
+++ src/pages/Recovery/index.js
@@ -1,19 +1,45 @@
 import React from 'react';
 import { 
     View,
-    Text
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StatusBar,
 } from 'react-native';
 
 import styles from './styles';
+import Background from '../../components/Background';
 
 export default function Recovery() {
 
   return (
-      <View style={styles.background}>
-        <Text>
-            Recuperar Senha
-        </Text>  
-      </View>
+        <Background>
+            <View style={styles.background}>
+                <StatusBar barStyle="light-content" backgroundColor="#FF0004" />
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>
+                        Recuperar senha
+                    </Text>
+                </View>
+                <View style={styles.body}>
+                    <Text style={styles.bodyText}>
+                        Digite seu e-mail para recuperar a senha
+                        </Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="   Digite seu email"
+                        autoCorrect={false}
+                        placeholderTextColor="#FFF"
+                        onChangeText={() => {}}
+                    />
+
+                    <TouchableOpacity style={styles.btnSend} onPress={() => {}}>
+                        <Text style={styles.btnSendText}>Enviar</Text>
+                    </TouchableOpacity>
+                </View>
+                
+            </View>
+        </Background>
     
   );
 }
