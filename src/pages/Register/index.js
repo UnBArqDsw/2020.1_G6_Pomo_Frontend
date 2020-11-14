b6fd8c1252d0bde453ac9e4705437ec852b115a8
Arthur <arthurarp@hotmail.com>
2020-11-14 18:20:35 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#54 Iniciando a estrutura da pagina de cadastro

---
+++ src/pages/Register/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { 
+    View,
+    Text,
+    
+} from 'react-native';
+
+import styles from './styles';
+
+export default function Register() {
+
+  return (
+    <View style={styles.background}>
+        <Text>Tela de cadastro</Text>
+    </View>
+    
+  );
+}
