df19984578c76a93c5b7e6e0b42b2f2b4b19f51c
Arthur <arthurarp@hotmail.com>
2020-11-14 15:22:53 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#53 Adicionando estilizacao da pagina de recuperar senha

--- src/pages/Recovery/styles.js
+++ src/pages/Recovery/styles.js
@@ -1,7 +1,51 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
 
 export default StyleSheet.create({
     background: {
         flex: 1,
+        // backgroundColor: '#fff4f4'
     }, 
+    header: {
+        flex: .13,
+        alignItems: 'center',
+        justifyContent: 'center',
+        // backgroundColor: '#FF005C',
+        borderBottomLeftRadius: 15,
+        borderBottomRightRadius: 15,
+    },
+    headerText: {
+        fontSize: windowWidth * .1,
+        color: '#fff',
+    },
+    body: {
+        flex: 1,
+        alignItems: 'center',
+        // marginLeft: 10
+    },
+    bodyText: {
+        marginTop: 20,
+    },
+    input: {
+        // backgroundColor: '#e1e1e1',
+        borderWidth: 1,
+        borderRadius: 5,
+        width: '85%',
+        height: '11%',
+        color: '#000',
+        marginTop: 20,
+    },
+    btnSend: {
+        marginTop: 15,
+        width: '40%',
+        height: '10%',
+        borderRadius: 20,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      btnSendText: {
+          color: '#FF005C'
+      },
 });
