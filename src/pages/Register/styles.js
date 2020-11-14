da0534c83239e6f69d7414fc1cf0a1c77d892f3c
Arthur <arthurarp@hotmail.com>
2020-11-14 19:03:29 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#54 Estilizando elementos da pagina de cadastro

--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -1,8 +1,41 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
 
 export default StyleSheet.create({
     background: {
         flex: 1,
     },
+    header:{
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headerText: {
+        alignItems: 'flex-end',
+        fontSize: windowWidth * .08,
+        color: '#FFF',
+        fontWeight: 'bold',
+      },
+      logoStyle: {
+          marginTop: 20,
+          width: windowWidth * 0.36,
+          height: windowWidth * 0.38,
+          marginBottom: 20,
+      },
+      bodyContainer: {
+          flex: 1,
+      },
+      body: {
+          alignItems: 'center'
+      },
+      descriptionWelcome: {
+        fontSize: 15,
+        color: '#FFF',
+        fontWeight: 'bold',
+      },
+      descriptionText: {
+        fontSize: 15,
+        color: '#FFF',
+      },
 
 });
