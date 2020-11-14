1be0e201b5fa98e9c06175fb7530ead1f39aa42f
Arthur <arthurarp@hotmail.com>
2020-11-14 02:57:13 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#53 Iniciando a estrutura da pagina de recuperar senha

---
+++ src/pages/Recovery/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { 
+    View,
+    Text
+} from 'react-native';
+
+import styles from './styles';
+
+export default function Recovery() {
+
+  return (
+      <View style={styles.background}>
+        <Text>
+            Recuperar Senha
+        </Text>  
+      </View>
+    
+  );
+}
