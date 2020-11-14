018d9b4c7a2d09447701a0d65efbad1043167a50
Arthur <arthurarp@hotmail.com>
2020-11-14 19:19:15 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#54 Estilizando as textinputs

--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -37,5 +37,28 @@ export default StyleSheet.create({
         fontSize: 15,
         color: '#FFF',
       },
+      inputsContainer: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginTop: 30,
+        marginBottom: 20,
+        // backgroundColor: '#FFF'
+      },
+      inputsContainerTextInput: {
+        flexDirection: 'row',
+        marginBottom: 15,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginLeft: -20,
+      },
+      inputsForm: {
+        backgroundColor: '#FFF',
+        width: '80%',
+        height: 50,
+        borderRadius: 7,
+        color: '#242940',
+      },
+
 
 });
