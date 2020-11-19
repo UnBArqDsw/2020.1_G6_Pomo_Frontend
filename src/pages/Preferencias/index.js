9497dc5c4c5b6fd7f0b8949c6078ed656714bd0c
Andre Eduardo <andre.eduardo11@hotmail.com>
2020-11-19 18:11:30 -0300

    [FEATURE] UnBArqDsw/2020.1_G6_Pomo#58 Pega usuario do redux

--- src/pages/Preferencias/index.js
+++ src/pages/Preferencias/index.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
 import Modal from './modal';
+import {useDispatch, useSelector} from 'react-redux';
+import {signOut} from '../../store/modules/auth/actions';
+
 import {
   Container,
   Title,
@@ -12,10 +15,16 @@ import {
   ContainerModal,
   ButtonSair,
 } from './styles';
-import {Keyboard} from 'react-native';
+
 export default function Preferencias() {
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.profile);
   const [modal, setModal] = useState(false);
-  const [test, setNewName] = useState('Andre Eduardo');
+  const [name, setNewName] = useState(profile.name);
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
   return (
     <Container>
       <Title>{'Preferências'}</Title>
@@ -26,18 +35,18 @@ export default function Preferencias() {
               'https://raw.githubusercontent.com/UnBArqDsw/2020.1_G6_Pomo/master/docs/img/andre.png',
           }}
         />
-        <Name>{test}</Name>
+        <Name>{name}</Name>
       </Header>
       <Body>
         <Button onPress={() => setModal(true)}>
           <TextButton>{'Editar perfil'}</TextButton>
         </Button>
-        <ButtonSair onPress={() => setModal(true)}>
+        <ButtonSair onPress={handleLogout}>
           <TextButton>{'Sair'}</TextButton>
         </ButtonSair>
       </Body>
       <ContainerModal>
-        <Modal show={modal} close={() => setModal(false)} user={test} />
+        <Modal show={modal} close={() => setModal(false)} user={name} />
       </ContainerModal>
     </Container>
   );
